7de4dbeeae1863e82cdce6d967f531217910d251
Patricia <patricia.femenias@gmail.com>
2023-11-27 22:03:42 +0100

    Add generalError middleware

--- src/server/middlewares/errors/errorsMiddlewares.ts
+++ src/server/middlewares/errors/errorsMiddlewares.ts
@@ -1,5 +1,9 @@
 import { type Request, type Response, type NextFunction } from "express";
+import debugCreator from "debug";
+import chalk from "chalk";
 import CustomError from "../../CustomError/CustomError.js";
+
+const debug = debugCreator("furbys: middlewares: errors");
 
 export const notFoundError = (
   _req: Request,
@@ -9,4 +13,18 @@ export const notFoundError = (
   const customError = new CustomError("Endpoint not found", 404);
 
   next(customError);
+};
+
+export const generalError = (
+  error: CustomError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  const statusCode = error.statusCode ?? 500;
+  const privateMessage = error.privateMessage ?? error.message;
+
+  debug(chalk.red("Error: ", privateMessage));
+
+  res.status(statusCode).json({ eror: error.message });
 };
