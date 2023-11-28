d5245b906669152a9e826a8d0c18321123692bb3
Patricia <patricia.femenias@gmail.com>
2023-11-28 10:29:50 +0100

    Add FurbyStructure interface

---
+++ src/features/furby/types.ts
@@ -0,0 +1,11 @@
+export interface FurbyStructure {
+  _id: string;
+  name: string;
+  type: string;
+  year: number;
+  generation: number;
+  language: string;
+  price: number;
+  howFoundit: string;
+  imageUrl: string;
+}
