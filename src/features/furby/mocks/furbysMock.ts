63be414e39c3424c69237ffff3c4951558978c94
Patricia <patricia.femenias@gmail.com>
2023-11-28 13:16:50 +0100

    Add getFurbys test

---
+++ src/features/furby/mocks/furbysMock.ts
@@ -0,0 +1,30 @@
+import { type FurbyStructure } from "../types";
+
+const furbysMock: FurbyStructure[] = [
+  {
+    _id: "6564a27d66ed505ce77a67d3",
+    name: "Peachy",
+    type: "baby",
+    year: 1999,
+    generation: 1,
+    language: "French",
+    price: 25,
+    howFoundit:
+      "I bought this Furby through Vinted and it arrived very clean and in perfect condition.",
+    imageUrl: "https://i.ibb.co/0FrzcXK/peachy.webp",
+  },
+  {
+    _id: "6564a27d66ed505ce77a67d4",
+    name: "Gizmo",
+    type: "special edition",
+    year: 2000,
+    generation: 5,
+    language: "English",
+    price: 84,
+    howFoundit:
+      "I stumbled upon the adorable Furby Gizmo while exploring a quirky vintage store in downtown London. Its bright colors and playful demeanor instantly caught my eye, and I couldn't resist bringing this delightful companion home with me.",
+    imageUrl: "https://i.ibb.co/ThY8Mdp/gizmo.webp",
+  },
+];
+
+export default furbysMock;
