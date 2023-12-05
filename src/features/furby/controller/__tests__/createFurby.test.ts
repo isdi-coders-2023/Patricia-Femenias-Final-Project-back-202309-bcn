ed34e2976c97c6845add3fa0d91c475dedb37abc
Patricia <patricia.femenias@gmail.com>
2023-12-05 21:11:04 +0100

    Add createFurby test

---
+++ src/features/furby/controller/__tests__/createFurby.test.ts
@@ -0,0 +1,78 @@
+import { type Request, type Response, type NextFunction } from "express";
+import { furbyMock } from "../../mocks/furbyMock";
+import type FurbysMongooseRepository from "../../repository/FurbysMongooseRepository";
+import { type FurbyRequestWithoutId } from "../../types";
+import FurbysController from "../FurbysController";
+import type CustomError from "../../../../server/CustomError/CustomError";
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("Given a FurbyController with a createFurby method", () => {
+  describe("When it receives a request with a new furby without id and a resposne", () => {
+    const req: Pick<FurbyRequestWithoutId, "body"> = {
+      body: furbyMock,
+    };
+
+    const res: Pick<Response, "status" | "json"> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockReturnThis(),
+    };
+
+    const next: NextFunction = jest.fn();
+
+    const furbysRepository: FurbysMongooseRepository = {
+      getFurbys: jest.fn(),
+      deleteFurby: jest.fn(),
+      addFurby: jest.fn().mockResolvedValue({ furbyMock }),
+    };
+
+    test("Then it should call its status method with the status code 201", async () => {
+      const furbyController = new FurbysController(furbysRepository);
+
+      await furbyController.addFurby(
+        req as FurbyRequestWithoutId,
+        res as Response,
+        next,
+      );
+
+      expect(res.status).toHaveBeenCalledWith(201);
+    });
+
+    test("Then it should call its json method with the Furby Peachy", async () => {
+      const furbyController = new FurbysController(furbysRepository);
+
+      await furbyController.addFurby(
+        req as FurbyRequestWithoutId,
+        res as Response,
+        next,
+      );
+
+      expect(res.json).toHaveBeenCalledWith({ furby: { furbyMock } });
+    });
+
+    test("Then, if it is an invalid request, it should call the next function with its error status 400 and the message 'Error adding a new Furby'", async () => {
+      const furbysRepository: FurbysMongooseRepository = {
+        getFurbys: jest.fn(),
+        deleteFurby: jest.fn(),
+        addFurby: jest.fn().mockRejectedValue(undefined),
+      };
+
+      const expectedError: Partial<CustomError> = {
+        message: "Error adding a new Furby",
+        statusCode: 400,
+      };
+
+      const furbyController = new FurbysController(furbysRepository);
+
+      await furbyController.addFurby(
+        req as FurbyRequestWithoutId,
+        res as Response,
+        next,
+      );
+
+      expect(next).toHaveBeenCalledWith(expect.objectContaining(expectedError));
+    });
+  });
+});
---
+++ src/features/furby/mocks/furbyMock.ts
@@ -0,0 +1,13 @@
+import { type FurbyStructureWithoutId } from "../types";
+
+export const furbyMock: FurbyStructureWithoutId = {
+  name: "Peachy",
+  type: "baby",
+  year: 1999,
+  generation: 1,
+  language: "French",
+  price: 25,
+  howFoundit:
+    "I bought this Furby through Vinted and it arrived very clean and in perfect condition.",
+  imageUrl: "https://i.ibb.co/0FrzcXK/peachy.webp",
+};
