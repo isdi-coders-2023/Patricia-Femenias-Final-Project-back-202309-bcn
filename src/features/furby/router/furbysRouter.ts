1bd0fcb3fafe8b41b119651f74bce96c3625f953
Patricia <patricia.femenias@gmail.com>
2023-12-08 20:58:31 +0100

    Add getFurbyById to Router

--- src/features/furby/router/furbysRouter.ts
+++ src/features/furby/router/furbysRouter.ts
@@ -7,13 +7,11 @@ import furbyValidation from "../schema/furbySchema.js";
 const furbysRouter = Router();
 
 const furbysRepository: FurbysRepository = new FurbysMongooseRepository();
-
 const furbysController = new FurbysController(furbysRepository);
 
 furbysRouter.get("/", furbysController.getFurbys);
-
 furbysRouter.delete("/:furbyId", furbysController.deleteFurby);
-
 furbysRouter.post("/create", furbyValidation, furbysController.addFurby);
+furbysRouter.get("/:furbyId", furbysController.getFurbyById);
 
 export default furbysRouter;
