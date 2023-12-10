d3695aa34c3f4ddc01cc8a230246407122916ed5
Patricia <patricia.femenias@gmail.com>
2023-12-10 18:25:45 +0100

    Add modifyFurby test

---
+++ src/features/furby/mocks/modifiedFurbyMock.ts
@@ -0,0 +1,13 @@
+import { type FurbyStructureWithoutId } from "../types";
+
+export const modifiedFurbyMock: FurbyStructureWithoutId = {
+  name: "Peachy",
+  type: "baby",
+  year: 1998,
+  generation: 1,
+  language: "French",
+  price: 25,
+  howFoundIt:
+    "I bought this Furby through Vinted and it arrived very clean and in perfect condition.",
+  imageUrl: "https://i.ibb.co/0FrzcXK/peachy.webp",
+};
